be2f5ff0c6d06f3a56e677e6dc5b5ceeca212113
Milan Brankovic <milanb@vast.com>
2014-11-04 19:44:46 +0100

    Design Demo Page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,9 +2,15 @@
 
 (function () {
     $(".toogle-menu").on('click', function() {
-        $(".drop-down-menu").toggleClass("expanded");
+        $(this).toggleClass("active");
+        $(".drop-down-menu").slideToggle(200);
     });
+
     $(window).on('scroll touchmove', function () {
         $(".header").toggleClass("sticky", $(document).scrollTop() > 356);
     });
+
+    $(".close").on('click', function() {
+        $(this).parent().fadeOut(400);
+    })
 })();
