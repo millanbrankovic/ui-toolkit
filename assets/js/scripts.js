4cc1af238aa0e686020ca3e2eb33dec78cf4c548
Milan Brankovic <milanb@vast.com>
2014-11-05 17:20:26 +0100

    Improve DropDown

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,49 @@
 // Scripts
 
 (function () {
-    $(".toogle-menu").on('click', function() {
-        $(this).toggleClass("active");
-        $(".main-nav").toggleClass("expanded");
+
+    openDropDowns($(".main-nav"));
+
+    function openDropDowns(ddMenus) {
+        ddMenus.children(".toogle-menu").on("click", function(e) {
+
+            e.stopPropagation();
+
+            var thisTrigger = $(this),
+                thisMenu = thisTrigger.parent(),
+                thisDDMenu = thisTrigger.next();
+
+            if (thisMenu.hasClass("expand")) {
+
+                thisMenu.removeClass("expand");
+
+                $(document).off("click");
+
+            } else {
+                ddMenus.removeClass("expand");
+
+                thisMenu.addClass("expand");
+
+                $(document).on("click", function() {
+                    ddMenus.removeClass("expand");
+                });
+            }
+        });
+    }
+
+
+
+    $(".has-drop-down").on('click', function(e) {
+        e.stopPropagation();
+
+        $(this).toggleClass("open");
     });
+
+    $(document).on('click', function() {
+        $(".has-drop-down").removeClass("open");
+    });
+
+
 
     $(window).on('scroll touchmove', function () {
         $(".header").toggleClass("sticky", $(document).scrollTop() > 356);
@@ -13,4 +52,6 @@
     $(".close").on('click', function() {
         $(this).parent().fadeOut(400);
     })
+
 })();
+
