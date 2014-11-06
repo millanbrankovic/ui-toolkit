2fd42d04e23e083522ca8fdbc6b50b6cbba257ba
Milan Brankovic <milanb@vast.com>
2014-11-06 10:58:45 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,3 @@
 // Scripts
-
-$(document).ready(function() {
-    $(".toogle-menu").on('click', function() {
-        $(".drop-down-menu").toggleClass("expanded");
-    });
-});
+//------------------------------------------------------------
+//
